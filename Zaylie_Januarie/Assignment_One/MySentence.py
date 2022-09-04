c969a278609c13d87dac346637f0723027c93820
Zaylie <zayliejanuarie10@gmail.com>
2022-09-04 14:38:44 +0200

    Zaylie

---
+++ Zaylie_Januarie/Assignment_One/MySentence.py
@@ -0,0 +1,5 @@
+sentence = input("Enter words: ")
+words=sentence.split()
+words.sort()
+for words in words:
+	print("sentence")
---
+++ Zaylie_Januarie/Assignment_One/monthOnCalender.py
@@ -0,0 +1,9 @@
+import calendar
+
+y =int(input("Input Year "))
+
+m =int(input("Input month "))
+
+print(calendar.month(y, m))
+
+
