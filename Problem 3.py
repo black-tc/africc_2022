fbb0ee3e706b07812243e5f66ff40534b1107a6e
DhimahiW <102658631+DhimahiW@users.noreply.github.com>
2022-09-09 23:14:34 +0400

    Add files via upload

---
+++ Problem 3.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+
+my_string = "Dhimahi Mahesh Walendra" 
+splitWords = my_string.split()
+
+splitWords.sort()
+
+print("The words in alphabetical order are...")
+for word in splitWords:
+     print(word)
+
